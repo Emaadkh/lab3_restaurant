ec383486a1cf3a40a47168b0ce5f1afee22c5f18
Emad <emadk571@gmail.com>
2023-02-02 14:48:42 -0500

    lab exercise

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const restaurantRouter = require('./routes/RestaurantRoutes.js');
+
+const app = express();
+app.use(express.json()); 
+
+
+mongoose.connect('mongodb+srv://Emaadkh:Emad2572@cluster0.f2ujd7x.mongodb.net/restaurant?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(success => {
+  console.log('Success Mongodb connection')
+}).catch(err => {
+  console.log('Error Mongodb connection')
+});
+
+app.use(restaurantRouter);
+
+app.listen(3000, () => { console.log('Server is running...') });
